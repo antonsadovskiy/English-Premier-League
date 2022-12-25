8a493ccd0de8227d50fbc4ec29a5ab8917ad0213
Anton Sadovskiy <antonsadovskiy6@gmail.com>
2022-12-25 14:08:50 +0300

    create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <p>Привет, меня зовут Антон</p>
+            <p>В этом курсе я буду изучать React JS</p>
+            <p>С помощью Димыча</p>
+            <p>Надеюсь мне повезет</p>
+            <Footer/>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to My first React App</h2>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>This is footer of my App</h2>
+        </div>
+    )
 }
 
 export default App;
